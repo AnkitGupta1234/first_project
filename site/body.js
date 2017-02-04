764aac56a811378736b4ef2c4184c4f58189ead5
ankit <ankitgankit123@gmail.com>
2017-02-04 12:12:10 +0530

    My site

---
+++ site/body.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+var i;
+for(i=0;i<1534;i+=180)
+{
+	  <svg >
+	  <polygon class="body_hex" points="i+30,50 i+90,50 i+120,100 i+90,150 i+30,150 i,100" />
+	  
+	  </svg>
+	  
+}
